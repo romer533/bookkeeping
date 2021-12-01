327f1984eacdead34a382322b9f98af7f1733699
RomanGoltsov <romer533@mail.ru>
2021-12-01 10:57:27 +0600

    Created WEB bookkeeping

---
+++ src/main/java/ru/bookkeeping/DemoWebappApplication.java
@@ -0,0 +1,11 @@
+package ru.bookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebappApplication.class, args);
+    }
+}
